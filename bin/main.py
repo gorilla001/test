bd12c74df7abcdf374dfa953fb49d2fc4203b271
aidenZou <aidenzou@gmail.com>
2015-10-12 18:11:14 +0800

    参数错误

--- bin/main.py
+++ bin/main.py
@@ -78,7 +78,7 @@ class PayWeixinHandler(AuthHandler):
         # 支付参数
         import random
 
-        params = make_order("测试支付", random.randint(1000000000000, 9999999999999), 1, self.request.remote_ip)
+        params = make_order(self, "测试支付", random.randint(1000000000000, 9999999999999), 1, self.request.remote_ip)
         log.error(params)
         self.render('pay_weixin.html', params=params)
 
@@ -90,7 +90,7 @@ class PayWeixinTestHandler(AuthHandler):
             # 支付参数
             import random
 
-            params = make_order("测试支付", random.randint(1000000000000, 9999999999999), 1, self.request.remote_ip)
+            params = make_order(self, "测试支付", random.randint(1000000000000, 9999999999999), 1, self.request.remote_ip)
             log.error(params)
             self.render('pay_weixin_test.html', params=params)
         except Exception as e:
